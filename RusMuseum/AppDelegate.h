e42d63dddf15eaf9f0d921ab1c205979844aa4f8
admin <maxpilgrim94@gmail.com>
2014-11-14 16:40:30 +0300

    Initial Commit

---
+++ RusMuseum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RusMuseum
+//
+//  Created by admin on 14.11.14.
+//  Copyright (c) 2014 crimsonjackets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RusMuseum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RusMuseum
+//
+//  Created by admin on 14.11.14.
+//  Copyright (c) 2014 crimsonjackets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
