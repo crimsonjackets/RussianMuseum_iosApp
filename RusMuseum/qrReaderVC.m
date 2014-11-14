1619ef2a4c8c779f0fd50447af3bf779995291d9
admin <maxpilgrim94@gmail.com>
2014-11-14 21:17:59 +0300

    qr scanner added. Local video.

--- RusMuseum/qrReaderVC.m
+++ RusMuseum/qrReaderVC.m
@@ -5,33 +5,109 @@
 //  Created by admin on 14.11.14.
 //  Copyright (c) 2014 crimsonjackets. All rights reserved.
 //
-
+#import <AVFoundation/AVFoundation.h>
 #import "qrReaderVC.h"
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface qrReaderVC ()
+@interface qrReaderVC () <AVCaptureMetadataOutputObjectsDelegate>
+{
+    AVCaptureSession *_session;
+    AVCaptureDevice *_device;
+    AVCaptureDeviceInput *_input;
+    AVCaptureMetadataOutput *_output;
+    AVCaptureVideoPreviewLayer *_prevLayer;
+    
+
+    IBOutlet UIView *viewForQR;
+}
 
 @end
 
 @implementation qrReaderVC
 
-- (void)viewDidLoad {
+
+-(void)viewWillAppear:(BOOL)animated{
+    [_session startRunning];
+}
+-(void)viewWillDisappear:(BOOL)animated{
+    [_session stopRunning];
+}
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    
+    _session = [[AVCaptureSession alloc] init];
+    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    
+    _input = [AVCaptureDeviceInput deviceInputWithDevice:_device error:&error];
+    if (_input) {
+        [_session addInput:_input];
+    } else {
+        NSLog(@"Error: %@", error);
+    }
+    
+    _output = [[AVCaptureMetadataOutput alloc] init];
+    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [_session addOutput:_output];
+    
+    _output.metadataObjectTypes = [_output availableMetadataObjectTypes];
+    
+    _prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+    _prevLayer.frame = viewForQR.bounds;
+//    _prevLayer.position = viewForQR.center;
+    _prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    [viewForQR.layer addSublayer:_prevLayer];
+    
+    [_session startRunning];
+    
+    [self.view bringSubviewToFront:viewForQR];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    CGRect highlightViewRect = CGRectZero;
+    AVMetadataMachineReadableCodeObject *barCodeObject;
+    NSString *detectionString = nil;
+    NSArray *barCodeTypes = @[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
+                              AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
+                              AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
+    
+    for (AVMetadataObject *metadata in metadataObjects) {
+        for (NSString *type in barCodeTypes) {
+            if ([metadata.type isEqualToString:type])
+            {
+                barCodeObject = (AVMetadataMachineReadableCodeObject *)[_prevLayer transformedMetadataObjectForMetadataObject:(AVMetadataMachineReadableCodeObject *)metadata];
+                highlightViewRect = barCodeObject.bounds;
+                detectionString = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
+                break;
+            }
+        }
+        
+        if (detectionString != nil)
+        {
+            [_session stopRunning];
+            //Video that should be played is determined by videoButton's tag of ab ExhibitImageView
+            NSLog(@"detected string == %@", detectionString);
+            NSString *filepath   =   [[NSBundle mainBundle] pathForResource:@"video.mp4" ofType:nil];
+            NSURL *movieURL = [NSURL fileURLWithPath:filepath];//[NSURL URLWithString:detectionString]
+            MPMoviePlayerViewController *movieController = [[MPMoviePlayerViewController alloc] initWithContentURL:movieURL];
+            [self presentMoviePlayerViewControllerAnimated:movieController];
+            [movieController.moviePlayer play];
+            break;
+        }
+//        else
+//            _label.text = @"(none)";
+    }
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
