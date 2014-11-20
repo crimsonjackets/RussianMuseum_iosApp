5145024d2965168919369bfde27e5cbd104db980
admin <maxpilgrim94@gmail.com>
2014-11-20 18:34:30 +0300

    exhibit views changed.

--- RusMuseum/ExhibitData.m
+++ RusMuseum/ExhibitData.m
@@ -11,13 +11,14 @@
 @implementation ExhibitData
 +(NSArray *)getExhibitDataForShowRoom:(int) showRoomId{
     if (showRoomId == 34){
-        return  @[@{@"title": @"Корабельная роща", @"author": @"И.И.Шишкин", @"info": @"", @"video":@"", @"image_name": @"korabel_roscha"},
-                  @{@"title": @"Покорение Сибири Ермаком", @"author": @"В.И.Суриков", @"info": @"", @"video":@"", @"image_name": @"pokorenie"},
-                  @{@"title": @"Переход Суворова через Альпы", @"author": @"В.И.Суриков", @"info": @"", @"video":@"", @"image_name": @"perehod_suvorova"}];
+        return  @[@{@"title": @"Девятый вал", @"author": @"И.К.Айвазовский", @"info": @"", @"video":@"", @"image_name": @"Devyatui_val"},
+                  @{@"title": @"Черный квадрат", @"author": @"К.С.Малевич", @"info": @"", @"video":@"", @"image_name": @"Black_square"},
+                  @{@"title": @"Вид памятника Петру I на Сенатской площади", @"author": @"В.И.Суриков", @"info": @"", @"video":@"", @"image_name": @"Petr_I"}];
     }
     if (showRoomId == 25){
-        return  @[@{@"title": @"Взятие снежного городка", @"author": @"В.И.Суриков", @"info": @"", @"video":@"", @"image_name": @"vzyatie"},
-                  @{@"title": @"Бурлаки на Волге", @"author": @"И.Е.Репин", @"info": @"", @"video":@"", @"image_name": @"burlaki"}];
+        return  @[@{@"title": @"Последний день Помпеи", @"author": @"К.П.Брюллов", @"info": @"", @"video":@"", @"image_name": @"startBackground"},
+                  @{@"title": @"Лунная ночь на Днепре", @"author": @"А.И.Куинджи", @"info": @"", @"video":@"", @"image_name": @"Lunnaya_noch"},
+                  @{@"title": @"Корабельная роща", @"author": @"И.И.Шишкин", @"info": @"", @"video":@"", @"image_name": @"korabel_roscha"}];
     }
     return nil;
 }
